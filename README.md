fc9aef9e90d1aa3f4d20b11805a614c3452ffba9
julian_idc <julianlu@wezhuiyi.com>
2020-08-22 14:44:12 +0800

    mod main and readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # SynthPDF  
 ----
-Generate Synthetic pdf files using custmizable content while recording elements coordinates for OCR objection detection
+Generate Synthetic pdf files using custmizable content while recording elements coordinates for OCR objection detection, elements including table, stamp, paragraph, list, title, footnote, etc. All fully customizable with config file. Outputs pdf, img, and json for necessary information.
+
+
+
+#![example1]
 
 ## Features
 
@@ -34,18 +38,34 @@ Generate Synthetic pdf files using custmizable content while recording elements 
 
 ## Requirements
 
-+ poppler-utils
-+ reportlab==3.5.28
-+ pdf2image==1.9.0
-+ tqdm==4.35.0
-+ matplotlib==3.1.1
-+ numpuy==1.17.1
-+ jsonschema==3.0.2
++ poppler-utils [linux]
++ reportlab==3.5.28 [python]
++ pdf2image==1.9.0 [python]
++ tqdm==4.35.0 [python]
++ matplotlib==3.1.1 [python]
++ numpuy==1.17.1 [python]
++ jsonschema==3.0.2 [python]
++ pyyaml==5.3.1 [python]
++ opencv==3.4.2 [python]
+
 
 ## Fonts
 
 Below fonts are non-standard such that extracted bounding box is not accurate:
 + langting-gbk
+
+## How to use
+1. Clone the repo to local
+2. Install required packages
+via pip
+```bash
+pip install -r requirements.txt
+```
+or conda [recommended]
+```bash
+conda install --file requirements.txt -c conda-forge
+```
+3. Update config file (config.yaml) to custom the pdf layout
 
 ## TODO
 
--- main.py
+++ main.py
@@ -29,30 +29,33 @@ if __name__ == '__main__':
 
 # ======================= Generate using customized elements  ============================
 
-# if __name__ == '__main__':    
-#     config =  load_yaml('config_large_table.yaml')
-#     cfg_runner = config['runner']
-#     run_parallel = cfg_runner['run_parallel']
-#     n_files = cfg_runner['n_files']
-#     n_processors = cfg_runner['num_processors']
-#     def runner(filename):
-#         sp = SynthPage(config, filename = filename)
-#         sp.add_spacer()
-#         sp.add_table()
-#         sp.add_spacer()
-#         sp.as_pdf()
-#         sp.as_img()
-#         #sp.annotate_box(save_img = True)
-#         sp.annotate_mask(save_img = True)        
-#     if run_parallel:
-#         filenames = [uuid.uuid4().hex + '.pdf' for _ in range(n_files)]
-#         SynthPage(config)._initialize() # make output folders to avoid parallel conflict 
-#         with Pool(n_processors) as p:
-#             _ = list(tqdm(p.imap_unordered(runner, filenames), total = n_files))
-#     else:
-#         for _ in tqdm(range(n_files)):
-#             filename = uuid.uuid4().hex + '.pdf'
-#             runner(filename)
+if __name__ == '__main__':    
+    config =  load_yaml('config.yaml')
+    cfg_runner = config['runner']
+    run_parallel = cfg_runner['run_parallel']
+    n_files = cfg_runner['n_files']
+    n_processors = cfg_runner['num_processors']
+    def runner(filename):
+        sp = SynthPage(config, filename = filename)
+        sp.add_spacer()
+        sp.add_title()
+        sp.add_table()
+        sp.add_paragraph()
+        sp.add_spacer()
+        sp.add_list()
+        sp.as_pdf()
+        sp.as_img()
+        sp.annotate_box(save_img = True)
+        #sp.annotate_mask(save_img = True)        
+    if run_parallel:
+        filenames = [uuid.uuid4().hex + '.pdf' for _ in range(n_files)]
+        SynthPage(config)._initialize() # make output folders to avoid parallel conflict 
+        with Pool(n_processors) as p:
+            _ = list(tqdm(p.imap_unordered(runner, filenames), total = n_files))
+    else:
+        for _ in tqdm(range(n_files)):
+            filename = uuid.uuid4().hex + '.pdf'
+            runner(filename)
 
 # ======================= test run ============================
 
@@ -61,10 +64,10 @@ if __name__ == '__main__':
 # #from elements.title import SynthTitle
 # config =  load_yaml('config.yaml')
 
-# # #Test Table 
-# # tb = SynthTable(config['table'])
-# # print(tb.nrows, tb.ncols)
-# # print(tb.table)
+# #Test Table 
+# tb = SynthTable(config['table'])
+# print(tb.nrows, tb.ncols)
+# print(tb.table)
 
 # # #Test Paragraph
 # # pa = SynthParagraph(config['paragraph'])
@@ -85,7 +88,8 @@ if __name__ == '__main__':
 # sp.annotate_box(save_img = True)
 # sp.annotate_mask(save_img = True)
 
-#Test Mixer 
-#m = PageMixer(config)
-#m.make()
-#print(m.page.doc.coords)
+# # #Test Mixer 
+# # m = PageMixer(config)
+# # m.make()
+# # print(m.page.doc.coords)
+
